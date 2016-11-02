3674dd81638a03f530257debe4728f1a1c828932
Leland Richardson <leland.m.richardson@gmail.com>
2016-11-02 10:30:40 -0700

    Fix Image Cache issues

--- Libraries/Image/RCTImageLoader.m
+++ Libraries/Image/RCTImageLoader.m
@@ -372,6 +372,9 @@ static UIImage *RCTResizeImageIfNeeded(UIImage *image,
     } else {
       // Use networking module to load image
       cancelLoad = [strongSelf _loadURLRequest:request
+                                          size:size
+                                         scale:scale
+                                    resizeMode:resizeMode
                                  progressBlock:progressHandler
                                completionBlock:completionHandler];
     }
@@ -387,6 +390,9 @@ static UIImage *RCTResizeImageIfNeeded(UIImage *image,
 }
 
 - (RCTImageLoaderCancellationBlock)_loadURLRequest:(NSURLRequest *)request
+                                              size:(CGSize)size
+                                             scale:(CGFloat)scale
+                                        resizeMode:(RCTResizeMode)resizeMode
                                      progressBlock:(RCTImageLoaderProgressBlock)progressHandler
                                    completionBlock:(void (^)(NSError *error, id imageOrData, NSString *fetchDate))completionHandler
 {
@@ -397,6 +403,17 @@ static UIImage *RCTResizeImageIfNeeded(UIImage *image,
                 request.URL.absoluteString);
     return NULL;
   }
+
+  UIImage *image = [[self imageCache] imageForUrl:request.URL.absoluteString
+                                                   size:size
+                                                  scale:scale
+                                             resizeMode:resizeMode
+                                           responseDate:@""];
+  if (image) {
+    completionHandler(nil, image, @"");
+    return ^{ };
+  }
+
 
   RCTNetworking *networking = [_bridge networking];
 
