89199b9534e509a16f46c4b9d538d68f6c572738
Yat Choi <yat.choi@airbnb.com>
2017-06-20 16:45:54 -0700

    [rn][geolocation] Provide option to request specific iOS location permissions
    [rn][geolocation] formatting

--- Libraries/Geolocation/Geolocation.js
+++ Libraries/Geolocation/Geolocation.js
@@ -28,6 +28,7 @@ type GeoOptions = {
   maximumAge: number,
   enableHighAccuracy: bool,
   distanceFilter: number,
+  locationPermissionIOS: string,
 }
 
 /**
@@ -142,6 +143,11 @@ var Geolocation = {
       subscriptions = [];
     }
   }
+};
+
+Geolocation.IOS_PERMISSIONS = {
+  WHEN_IN_USE: "locationPermissionIOSWhenInUse",
+  ALWAYS: "locationPermissionIOSAlways",
 };
 
 module.exports = Geolocation;
